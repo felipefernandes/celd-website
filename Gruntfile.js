585e6a4d68fb1cdfaf80b5b0caae809a9579be64
Felipe Fernandes <felipefernandesweb@gmail.com>
2018-08-07 13:44:09 -0300

    Atualizações na estrutura, adição das tasks grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,72 +1,66 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-     // configurações das tasks
-     less: {
-       'assets/css/main.css': 'builds/less/main.less'
-     }, //less
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-     ngAnnotate: {
-          options: {
-              singleQuotes: true
-          },
-          app: {
+        sass: {
+            dist: {
+                options: {
+                    sourcemap: true
+                },
+                files: {
+                    'src/styles/main.css': 'src/styles/main.scss'
+                }
+            }
+        }, //sass
+
+        uglify: {
+            js: { //target
+                src: ['src/scripts/main.js'],
+                dest: 'assets/js/main.js'
+            }
+        }, //uglify
+
+        cssmin: {
+            options: {
+              shorthandCompacting: false,
+              roundingPrecision: -1,
+              ext: 'min.css'
+            },
+            target: {
               files: {
-                  'assets/js/main.js': ['builds/js/main.js']
+                'assets/css/main.css': 'assets/css/main.css'
               }
-          }
-      }, //ngAnnotate
+            }
+          }, //cssmin
 
-      uglify: {
-          js: { //target
-              src: ['builds/js/main.js'],
-              dest: 'assets/js/main.js'
-          }
-      }, //uglify
-
-      cssmin: {
-        options: {
-          shorthandCompacting: false,
-          roundingPrecision: -1
+        concat: {
+            dist: {
+                src: "src/scripts/**/*"
+              , dest: "assets/js/main.js"
+            }
         },
-        target: {
-          files: {
-            'assets/css/main.css': 'assets/css/main.css'
-          }
-        }
-      }, //cssmin
 
-      concat: {
-        dist: {
-            src: "builds/js/**/*"
-          , dest: "assets/js/main.js"
-        }
-      },
+        watch: {
+            css: {
+                files: [
+                    '**/*/*.scss', 
+                    '**/*/*.js'
+                ],
+                tasks: ['concat', 'sass', 'cssmin', 'uglify']
+            }
+        }, 
 
-      watch : {
-        dist : {
-          files : [
-            'builds/js/**/*',
-            'builds/css-less/**/*'
-          ],
+    });
 
-          tasks : [ 'concat', 'less', 'cssmin', 'ngAnnotate', 'uglify' ]
-        }
-      } // watch
+    // Load Grunt Plugins
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
+    // Register Grunt Tasks
+    grunt.registerTask('default', ['concat', 'sass', 'cssmin', 'uglify']);
 
-  });
-
-  // carrega plugins
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-ng-annotate');
-  grunt.loadNpmTasks( 'grunt-contrib-watch' );
-
-  // Tarefa para o comando default
-  grunt.registerTask('default', ['concat', 'less', 'cssmin', 'ngAnnotate', 'uglify']);
-
-  // Tarefa para Watch
-  grunt.registerTask( 'w', [ 'watch' ] );
 };
+
